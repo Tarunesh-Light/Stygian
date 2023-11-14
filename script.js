9010bfe40dcc77cc9ce277a81d1dfd407ab81544
Tarunesh-Light <workassignments796796@gmail.com>
2023-11-14 22:50:07 +0530

    Stygian

---
+++ script.js
@@ -0,0 +1,31 @@
+var t1 = gsap.timeline()
+
+t1.from(".title,.product,.catalogue,.template,.contact,.button",{
+    y:-100,
+    duration:1,
+    stagger:0.2
+})
+
+t1.from(".firstquote,.secquote,.thirdquote",{
+    y:100,
+    opacity:0,
+    stagger:0.4
+})
+
+t1.from("img",{
+    scale:0,
+    opacity:0,
+    stagger:0.3
+})
+
+t1.from(".scroll",{
+    scale:0,
+    opacity:0
+})
+
+t1.to(".scroll",{
+    y:15,
+    repeat: -1,
+    duration:0.7,
+    yoyo: true
+})
